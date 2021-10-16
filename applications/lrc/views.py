8ee494352007ecc39876f578b2104457009aef85
bhdrkcr <bahadir2kacar@gmail.com>
2021-10-16 19:25:02 +0300

    user view

--- applications/lrc/views.py
+++ applications/lrc/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+# Third Party
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+# Local Folder
+from .models import Register, User
+from .serializer import RegisterSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing the users.
+    """
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy', 'list']:
+            self.permission_classes = [permissions.IsAuthenticated]
+        elif self.action in ['create']:
+            self.permission_classes = [permissions.IsAdminUser]
+        elif self.action in ['retrieve']:
+            if self.request.user.id == int(self.kwargs['pk']):
+                self.permission_classes = [permissions.IsAuthenticated]
+            else:
+                self.permission_classes = [permissions.IsAdminUser]
+        return super(UserViewSet, self).get_permissions()
+
+    def get_queryset(self):
+        if self.action == 'list':
+            if self.request.user.is_superuser:
+                return User.objects.all()
+            else:
+                return self.queryset.filter(id=self.request.user.id)
+        else:
+            return super().get_queryset()
+
+
+class RegisterViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing the users.
+    """
+
+    serializer_class = RegisterSerializer
+    queryset = Register.objects.all()
