0c6dec537104ae5d282c73a9d389c8393e091aa7
bhdrkcr <bahadir2kacar@gmail.com>
2021-10-16 19:25:19 +0300

    connect router to urls

--- config/urls.py
+++ config/urls.py
@@ -21,9 +21,18 @@ from django.urls import include, path
 
 # Third Party
 import debug_toolbar
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("lrc.urls", namespace="lrc")),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
